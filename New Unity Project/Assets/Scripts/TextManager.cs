067578c5dce834987e81148bfbb6adee0fd12011
Anyoninetail <Yonghopark0315@gmail.com>
2022-10-04 15:41:30 +0900

    추가
    텍스트 형식 생성

---
+++ New Unity Project/Assets/Scripts/TextManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour
+{
+    public string originText;
+    public Text dialog;
+    public float textSpeed;
+
+    void Start()
+    {
+
+    }
+}
